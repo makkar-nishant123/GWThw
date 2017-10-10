893fc8b95857564d0bdc09ebd1789a48506605ab
nmakkar@xavient.com <nmakkar@xavient.com>
2017-10-10 17:49:38 +0530

    first commit

---
+++ src/helloWorld/client/HelloWorld.java
@@ -0,0 +1,19 @@
+package helloWorld.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class HelloWorld implements EntryPoint {
+	/**
+	
+	/**
+	 * This is the entry point method.
+	 */
+		public void onModuleLoad() {
+		      Window.alert("Hello, World!");
+		   }
+	}
+
